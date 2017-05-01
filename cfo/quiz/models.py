8b63a45c7bbf1ae1c475d093099471b7f07ca795
Wellington Uellner <wellingtono@gmail.com>
2017-05-01 17:24:45 -0300

    ajeitando detalhes da geracao de perguntas do quiz

--- cfo/quiz/models.py
+++ cfo/quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Quiz(models.Model):
@@ -6,6 +7,7 @@ class Quiz(models.Model):
         Classe que representa um quiz (conjunto de perguntas e respostas).
     """
     course = models.ForeignKey('course.Course', verbose_name="Curso")
+    unit = models.ForeignKey('course.Unit', blank=True, null=True, verbose_name="Unidade")
     questions = models.ManyToManyField('quiz.Question', blank=True, verbose_name='Questões')
 
     def __str__(self):
@@ -32,6 +34,14 @@ class Answer(models.Model):
         verbose_name_plural = "Alternativas"
 
 
+class QuestionsManager(models.Manager):
+    def random(self, sample=1, units=[1]):
+        random_index = random.sample(list(self.filter(units__in=units)), sample)
+        if sample > len(random_index):
+            return None
+        return self.filter(pk__in=list(map(lambda e: e.id, random_index))).all()
+
+
 class Question(models.Model):
     u"""
         Classe que representa as questões.
@@ -39,6 +49,7 @@ class Question(models.Model):
     description = models.TextField(verbose_name="Pergunta")
     comment = models.TextField(null=True, blank=True, verbose_name="Comentário")
     units = models.ManyToManyField('course.Unit', blank=True, verbose_name='Unidades')
+    objects = QuestionsManager()
 
     def __str__(self):
         return self.description
@@ -46,3 +57,19 @@ class Question(models.Model):
     class Meta:
         verbose_name = "Questão"
         verbose_name_plural = "Questões"
+
+    # @classmethod
+    # def get_questions_unit_quiz(cls, unit, sample=10):
+    #     u"""
+    #         Get random questions to unit quiz.
+    #     """
+    #     if not unit:
+    #         return False
+    #
+    #     course_progress = cls.objects.filter(
+    #         course=self,
+    #         student=student,
+    #         activity=self.get_start_activity()
+    #     )
+    #     course_progress.save()
+    #     return course_progress.activity
--- cfo/quiz/views.py
+++ cfo/quiz/views.py
@@ -3,15 +3,16 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from annoying.decorators import render_to
 from .models import Quiz
+from ..course.models import Unit
 
 
 @render_to('quiz.html')
 @login_required
 def start_unit_quiz(request, unit_id):
-    # activity = get_object_or_404(Activity, id=id)
+    unit = get_object_or_404(Unit, id=unit_id)
     return {
         'user_logout': reverse('logout_view'),
         'data': {
-            'course': 'teste',
+            'unit': unit,
         }
     }
