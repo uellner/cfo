608199e1180fd4ba19f9ade02301818a4d68478c
Wellington Uellner <wellingtono@gmail.com>
2017-05-28 18:03:02 -0300

    load current question on screen

--- cfo/quiz/views.py
+++ cfo/quiz/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from annoying.decorators import render_to
-from .models import Quiz, QuizProgress
+from .models import Quiz, QuizProgress, QuizQuestion, Answer
 from ..course.models import Unit
 
 
@@ -41,10 +41,20 @@ def start_unit_quiz(request, unit_id):
 @render_to('quiz.html')
 @login_required
 def resume_quiz(request, quiz_progress_id):
-    quiz_progress = QuizProgress.objects.filter(id=quiz_progress_id)
+    quiz_progress = get_object_or_404(QuizProgress, id=quiz_progress_id)
+    answers = None
+    if quiz_progress:
+        current_question = QuizQuestion.objects.filter(
+            quiz=quiz_progress.quiz,
+            number=quiz_progress.answer + 1
+        ).all()
+        current_question = current_question and current_question[0]
+        answers = Answer.objects.filter(question=current_question.question).all()
     return {
         'user_logout': reverse('logout_view'),
         'data': {
-            'quiz_progress': quiz_progress
+            'quiz_progress': quiz_progress,
+            'current_question': current_question,
+            'answers': answers
         }
     }
